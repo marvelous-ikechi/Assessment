547bad1ee1604ad860338508bc42cc614840939b
marvelous-ikechi <marvelike7@gmail.com>
2024-08-16 13:14:19 +0100

    handle next and previous track player

--- src/screens/MusicPlayer.tsx
+++ src/screens/MusicPlayer.tsx
@@ -1,16 +1,22 @@
-import React, {FunctionComponent, useCallback} from 'react';
+import React, {FunctionComponent, useCallback, useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 import {NavigatorParams} from '../navigation/types/navigationTypes';
 import {styles} from '../utils/styles';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Slider from '@react-native-community/slider';
+import {usePlaylistStore} from '../zustand/store';
 
 type Props = NativeStackScreenProps<NavigatorParams, 'MusicPlayer'>;
 const MusicPlayer: FunctionComponent<Props> = ({navigation, route}) => {
-  const {title, duration} = route.params;
+  const {title, duration, id} = route.params;
   const [currentTrackDuration, setCurrentTrackDuration] =
     React.useState<number>(0);
+
+  const setCurrentTrack = usePlaylistStore(item => item.setCurrentTrack);
+  const setTrackQueue = usePlaylistStore(item => item.setTrackQueue);
+  const selectedPlayList = usePlaylistStore(item => item.selectedPlayList);
+  const currentTrack = usePlaylistStore(item => item.currentTrack);
 
   const playSong = useCallback(() => {
     const interval = setInterval(() => {
@@ -19,6 +25,58 @@ const MusicPlayer: FunctionComponent<Props> = ({navigation, route}) => {
     }, 1000);
     return () => clearInterval(interval);
   }, [currentTrackDuration, duration]);
+
+  const next = useCallback(() => {
+    const currentTrackIndex = selectedPlayList?.songs?.findIndex(
+      item => item.id === currentTrack?.id,
+    );
+    if (selectedPlayList?.songs && currentTrackIndex !== undefined) {
+      // Check if the current track is the last one in the playlist
+      if (currentTrackIndex < selectedPlayList.songs.length - 1) {
+        const nextTrack = selectedPlayList.songs[currentTrackIndex + 1];
+        setCurrentTrack(nextTrack);
+        setCurrentTrackDuration(0);
+        setTrackQueue(selectedPlayList?.songs);
+      } else {
+        // Stop playback if it's the last track in the queue
+        setCurrentTrackDuration(duration);
+      }
+    }
+  }, [
+    currentTrack,
+    selectedPlayList,
+    setCurrentTrack,
+    setTrackQueue,
+    duration,
+  ]);
+
+  const previous = useCallback(() => {
+    const currentTrackIndex = selectedPlayList?.songs?.findIndex(
+      item => item.id === currentTrack?.id,
+    );
+    if (selectedPlayList?.songs && currentTrackIndex !== undefined) {
+      const previousTrack =
+        selectedPlayList?.songs[currentTrackIndex - 1] ||
+        selectedPlayList?.songs[selectedPlayList?.songs?.length - 1];
+      setCurrentTrack(previousTrack);
+      setCurrentTrackDuration(0);
+      setTrackQueue(selectedPlayList?.songs);
+    }
+  }, [currentTrack, selectedPlayList, setCurrentTrack, setTrackQueue]);
+
+  useEffect(() => {
+    setCurrentTrack({
+      title,
+      duration,
+      id,
+    });
+  }, [title, duration, id, setCurrentTrack]);
+
+  useEffect(() => {
+    if (currentTrackDuration >= duration) {
+      next();
+    }
+  }, [currentTrackDuration, duration, next]);
 
   return (
     <SafeAreaView style={styles.wrapper}>
@@ -30,7 +88,7 @@ const MusicPlayer: FunctionComponent<Props> = ({navigation, route}) => {
             size={30}
             color="black"
           />
-          <Text>{title}</Text>
+          <Text>{currentTrack?.title}</Text>
           <View />
         </View>
         <Slider
@@ -43,14 +101,24 @@ const MusicPlayer: FunctionComponent<Props> = ({navigation, route}) => {
           value={currentTrackDuration}
         />
         <View style={styles.row}>
-          <MaterialCommunityIcon name="skip-previous" size={30} color="black" />
+          <MaterialCommunityIcon
+            onPress={previous}
+            name="skip-previous"
+            size={30}
+            color="black"
+          />
           <MaterialCommunityIcon
             onPress={() => playSong()}
             name="play"
             size={30}
             color="black"
           />
-          <MaterialCommunityIcon name="skip-next" size={30} color="black" />
+          <MaterialCommunityIcon
+            onPress={next}
+            name="skip-next"
+            size={30}
+            color="black"
+          />
         </View>
       </View>
     </SafeAreaView>
