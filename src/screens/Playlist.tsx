4792255b5e0a390983103e47f92b5604880ce87a
marvelous-ikechi <marvelike7@gmail.com>
2024-08-16 10:52:48 +0100

    playlist component created

--- src/screens/Playlist.tsx
+++ src/screens/Playlist.tsx
@@ -1,30 +1,65 @@
 import React, {FunctionComponent} from 'react';
-import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ListRenderItem,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import {NativeStackScreenProps} from 'react-native-screens/lib/typescript/native-stack/types';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {NavigatorParams} from '../navigation/types/navigationTypes';
+import {styles} from '../utils/styles';
+import {IPlaylist, usePlaylistStore} from '../zustand/store';
 
-const Playlist: FunctionComponent = () => {
+type Props = NativeStackScreenProps<NavigatorParams, 'Playlist'>;
+
+const Playlist: FunctionComponent<Props> = ({navigation}) => {
+  const renderItem: ListRenderItem<IPlaylist> = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => navigation.navigate('PlaylistDetails', item)}
+        style={localstyles.playListItem}>
+        <Text>{item.title}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const playlistData = usePlaylistStore(item => item.playlists);
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <View style={styles.row}>
           <Text>Playlist</Text>
-          <MaterialCommunityIcon name="plus-circle" size={30} color="black" />
+          <MaterialCommunityIcon
+            onPress={() => navigation.navigate('AddPlayList')}
+            name="plus-circle"
+            size={30}
+            color="black"
+          />
         </View>
+        <FlatList
+          data={playlistData}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 10,
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+const localstyles = StyleSheet.create({
+  playListItem: {
+    borderWidth: 1,
+    padding: 4,
+    height: 40,
+    marginHorizontal: 5,
+    marginVertical: 5,
+    justifyContent: 'center',
+    borderRadius: 5,
   },
 });
 
