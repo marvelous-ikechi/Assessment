7675e5edffae707f5c32133ab398b25a11be2c23
marvelous-ikechi <marvelike7@gmail.com>
2024-08-16 14:12:15 +0100

    created bottom player

--- src/components/BottomPlayer.tsx
+++ src/components/BottomPlayer.tsx
@@ -1,16 +1,77 @@
-import React, {FunctionComponent} from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {styles} from '../utils/styles';
+import React, {FunctionComponent, useCallback, useRef, useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import MaterialCommunityIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {usePlaylistStore} from '../zustand/store';
 
-const MusicPlayer: FunctionComponent = () => {
+const BottomTrackPlayer: FunctionComponent = () => {
+  const currentTrack = usePlaylistStore(item => item.currentTrack);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+  const setIsCurrentlyPlaying = usePlaylistStore(
+    item => item.setIsCurrentlyPlaying,
+  );
+  const isCurrentlyPlaying = usePlaylistStore(item => item.isCurrentlyPlaying);
+  const isPaused = usePlaylistStore(item => item.isPaused);
+  const setIsPaused = usePlaylistStore(item => item.setIsPaused);
+
+  const [trackDuration, setTrackDuration] = useState<number>(0);
+
+  const startPlaying = useCallback(() => {
+    intervalRef.current = setInterval(() => {
+      setTrackDuration(prev => {
+        if (prev < trackDuration) {
+          return prev + 1;
+        } else {
+          clearInterval(intervalRef.current!);
+          return prev;
+        }
+      });
+    }, 1000);
+  }, [trackDuration]);
+
+  const playSong = useCallback(() => {
+    setIsCurrentlyPlaying(true);
+    setIsPaused(false);
+    startPlaying();
+  }, [setIsCurrentlyPlaying, setIsPaused, startPlaying]);
+
+  const pauseSong = useCallback(() => {
+    setIsPaused(true);
+    setIsCurrentlyPlaying(false);
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }, [setIsPaused, setIsCurrentlyPlaying]);
+
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <MaterialCommunityIcon name="chevron-left" size={30} color="black" />
-      </View>
-    </SafeAreaView>
+    <View style={localstyles.playerContainer}>
+      <Text>{currentTrack?.title}</Text>
+      {isCurrentlyPlaying ? (
+        <MaterialCommunityIcon
+          onPress={pauseSong}
+          name="pause"
+          size={30}
+          color={'black'}
+        />
+      ) : (
+        <MaterialCommunityIcon
+          onPress={playSong}
+          name="play"
+          size={30}
+          color="black"
+        />
+      )}
+    </View>
   );
 };
 
-export default MusicPlayer;
+const localstyles = StyleSheet.create({
+  playerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 50,
+    backgroundColor: 'grey',
+  },
+});
+
+export default BottomTrackPlayer;
